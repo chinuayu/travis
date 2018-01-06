3f515c26c72783d7a861b27a9a3d65af8c013b0e
Gaurav Bansal <gauravbansal9999@gmail.com>
2018-01-06 15:00:47 +0530

    travis files

---
+++ upload-apk.sh
@@ -0,0 +1,26 @@
+#create a new directory that will contain out generated apk
+
+mkdir $HOME/buildApk/ 
+#copy generated apk from build folder to the folder just created
+
+cp -R app/build/outputs/apk/app-debug.apk $HOME/android/
+#go to home and setup git  
+
+cd $HOME
+git config --global user.email "bgaurav7@github.com"
+git config --global user.name "Gaurav Bansal" 
+#clone the repository in the buildApk folder
+
+git clone --quiet --branch=master  https://bgaurav7:$GITHUB_API_KEY@github.com/bgaurav7/travis  master > /dev/null
+#go into directory and copy data we're interested
+
+cd master  cp -Rf $HOME/android/* .
+#add, commit and push files
+
+git add -f .
+git remote rm origin
+git remote add origin https://bgaurav7:$GITHUB_API_KEY@github.com/bgaurav7/travis.git
+git add -f .
+git commit -m "Travis build $TRAVIS_BUILD_NUMBER pushed [skip ci] "
+git push -fq origin master > /dev/null
+echo -e "Done\n"
