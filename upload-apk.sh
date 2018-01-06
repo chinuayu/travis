2e8c5e72aa450c918727f9e070806fe96ffede6e
Gaurav Bansal <bgaurav7@github.com>
2018-01-06 15:40:05 +0530

    upload apk travis fixed

--- upload-apk.sh
+++ upload-apk.sh
@@ -1,22 +1,22 @@
 #create a new directory that will contain out generated apk
+mkdir $HOME/android/ 
 
-mkdir $HOME/buildApk/ 
 #copy generated apk from build folder to the folder just created
-
 cp -R app/build/outputs/apk/debug/app-debug.apk $HOME/android/
-#go to home and setup git  
 
+#go to home and setup git  
 cd $HOME
 git config --global user.email "bgaurav7@github.com"
 git config --global user.name "Gaurav Bansal" 
 #clone the repository in the buildApk folder
 
 git clone --quiet --branch=master  https://bgaurav7:$GITHUB_API_KEY@github.com/bgaurav7/travis  master > /dev/null
+
 #go into directory and copy data we're interested
+cd master  
+cp -Rf $HOME/android/* .
 
-cd master  cp -Rf $HOME/android/* .
 #add, commit and push files
-
 git add -f .
 git remote rm origin
 git remote add origin https://bgaurav7:$GITHUB_API_KEY@github.com/bgaurav7/travis.git
