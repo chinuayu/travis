c5d8445bab9c31fc0d7b9dea46efb2931a080c67
BESTDEALFINANCE\ayushi.gupta <ayushi.gupta@rubique.com>
2018-01-07 18:28:50 +0530

    Changes

--- app/src/main/java/in/tesseract/test/MainActivity.java
+++ app/src/main/java/in/tesseract/test/MainActivity.java
@@ -17,7 +17,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        System.out.println("Hey");
+        System.out.println("Hey I am ");
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
